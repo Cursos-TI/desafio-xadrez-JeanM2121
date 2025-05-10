6128d8d5e8496e6ea0703790d07c97507205da6c
JeanM2121 <jean.mdossantos21@gmail.com>
2025-05-10 18:25:08 -0300

    Create desafioxadreznovato.c

---
+++ desafioxadreznovato.c
@@ -0,0 +1,87 @@
+#include <stdio.h>   
+#include <stdlib.h>
+#include <conio.h>
+ 
+int main() {
+        printf("torre\n");
+   for ( int i = 0; i < 5; i++) {
+
+        printf("Cima \n");
+   }
+    for ( int i = 0; i < 5; i++) {
+
+        printf("Direita \n");
+    }
+    
+    for ( int i = 0; i < 5; i++) {
+
+        printf("Esquerda \n");
+    }
+    
+    for ( int i = 0; i < 5; i++) {
+
+        printf("Baixo \n");
+    }
+        printf("bispo\n");
+   for ( int i = 0; i < 5; i++) {
+
+        printf("Diagonal cima direita\n");
+   }
+   
+   for ( int i = 0; i < 5; i++) {
+
+        printf("Diagonal baixo direita \n");
+    }
+
+    for ( int i = 0; i < 5; i++) {
+
+        printf("Diagonal cima esquerda \n");
+    }
+    
+    for ( int i = 0; i < 5; i++) {
+
+        printf("Diagonal baixo esquerda \n");
+    }
+
+        printf("rainha\n");
+   for ( int i = 0; i < 5; i++) {
+
+        printf("Cima \n");
+   }
+          
+   for ( int i = 0; i < 5; i++) {
+
+         printf("baixo \n");
+        
+   }
+   for ( int i = 0; i < 5; i++) {
+
+    printf("Diagonal cima direita\n");
+}
+
+for ( int i = 0; i < 5; i++) {
+
+    printf("Diagonal baixo direita \n");
+}
+
+for ( int i = 0; i < 5; i++) {
+
+    printf("Diagonal cima esquerda \n");
+}
+
+for ( int i = 0; i < 5; i++) {
+
+    printf("Diagonal baixo esquerda \n");
+}
+for ( int i = 0; i < 5; i++) {
+
+    printf("Direita\n");
+}
+
+for ( int i = 0; i < 5; i++) {
+
+    printf("Esquerda \n");
+}
+
+    return 0;
+}
