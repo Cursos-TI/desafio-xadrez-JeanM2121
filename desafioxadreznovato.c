cd8d17d410722479770da2e2c72481cb94573b39
JeanM2121 <jean.mdossantos21@gmail.com>
2025-05-19 12:51:26 -0300

    Update desafioxadreznovato.c

--- desafioxadreznovato.c
+++ desafioxadreznovato.c
@@ -1,52 +1,97 @@
-#include <stdio.h>
-
+#include <stdio.h>   
+#include <stdlib.h>
+#include <conio.h>
+  
+ 
+//DESENVOLVI O CODIGO ME BASEANDO EM TODAS AS POSIÇÕES POSSIVEIS QUE CADA PEÇA POSSA FAZER PULANDO 5 CASAS...
 int main() {
-    int bispo = 0;
-    int cavalo = 0;
-    int rainha = 0;
-    int torre = 0;
 
-    for (int i = 1; i <= 10; i++) {
-        for (int j = 1; j <= 10; j++) {
-            printf("%d\t", i * j);
-        }
-        printf("\n");
+    printf("****TORRE****\n"); //Movimentos possiveis da torre!!
+   for ( int i = 0; i < 5; i++) {
+
+        printf("Cima \n");
+   }
+    for ( int i = 0; i < 5; i++) {
+
+        printf("Direita \n");
     }
-
-    printf("Movimentando o Bispo (5 casas):\n");
-
-    do {
-        printf("Cima, Direita\n");  
-        bispo++; 
-    } while (bispo < 5); 
-
-    printf("Movimentando o torre (5 casas):\n");
-
-    do {
-        printf("Cima\n");  
-        torre++; 
-    } while (torre < 5); 
-
-    printf("Movimentando a rainha (5 casas):\n");
-
-    do {
-        printf("esquerda\n");  
-        rainha++; 
-    } while (rainha < 5); 
-
-    printf("movimentando o cavalo \n");
     
-    while(cavalo --){
-        for (int i = 0; i < 2; i++){
-            printf("Baixo... \n");
-            
-        }
-        printf("Esquerda... \n");
-        
+    for ( int i = 0; i < 5; i++) {
+
+        printf("Esquerda \n");
+    }
+    
+    for ( int i = 0; i < 5; i++) {
+
+        printf("Baixo \n");
+    }
+        printf("****BISPO****\n"); //Movimentos possiveis do bispo!!!
+   for ( int i = 0; i < 5; i++) {
+
+        printf("cima, direita\n");
+   }
+   
+   for ( int i = 0; i < 5; i++) {
+
+        printf(" baixo, direita \n");
     }
 
+    for ( int i = 0; i < 5; i++) {
 
+        printf(" cima, esquerda \n");
+    }
+    
+    for ( int i = 0; i < 5; i++) {
+
+        printf(" baixo, esquerda \n");
+    }
+
+        printf("****RAINHA****\n"); //Movimentos possiveis da rainha!!!
+   for ( int i = 0; i < 5; i++) {
+
+        printf("Cima \n");
+   }
+          
+   for ( int i = 0; i < 5; i++) {
+
+         printf("baixo \n");
+        
+   }
+   for ( int i = 0; i < 5; i++) {
+
+    printf(" cima, direita\n");
+}
+
+for ( int i = 0; i < 5; i++) {
+
+    printf(" baixo, direita \n");
+}
+
+for ( int i = 0; i < 5; i++) {
+
+    printf(" cima, esquerda \n");
+}
+
+for ( int i = 0; i < 5; i++) {
+
+    printf("baixo, esquerda \n");
+}
+for ( int i = 0; i < 5; i++) {
+
+    printf("Direita\n");
+}
+
+for ( int i = 0; i < 5; i++) {
+
+    printf("Esquerda \n");
+}
 
     return 0;
+
+
 }
+
+
+
+
     
