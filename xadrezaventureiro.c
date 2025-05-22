8d30f61f5b86e0eb4c8eb13940889c2b840188db
JeanM2121 <jean.mdossantos21@gmail.com>
2025-05-22 13:27:18 -0300

    Update xadrezaventureiro.c

--- xadrezaventureiro.c
+++ xadrezaventureiro.c
@@ -35,7 +35,7 @@ int main() {
     printf("Movimento do Cavalo:\n");
     for (int i = 0; i <= 2; i++) {
         for (int j = 0; j <= 1; j++) {
-            printf("baixo \n");
+        printf("baixo \n");
         }
         printf("esquerda \n");
         break;
